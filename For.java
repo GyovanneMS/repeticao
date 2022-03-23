51a250d8d60c03cf31244545a2b60fb31922fdf1
Gyovanne <22102360@CFP127ALUNO.LOCAL>
2022-03-23 10:45:21 -0300

    atualização

---
+++ For.java
@@ -0,0 +1,16 @@
+
+
+
+class For{
+    public static void main(String args[]){
+
+        for(int i=1; i <= 13; i=i+3){
+            if(i == 13){
+                System.out.print(i + ".");
+            }
+            else{
+            System.out.print(i + ",");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # repeticao
+git status 
+git add . 
+git status 
+git commit -m "atualização" 
+git status git push 
+git status
