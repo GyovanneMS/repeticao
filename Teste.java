53f7c4efef1d648cc2901a539287a87c163b46c8
Gyovanne <22102360@CFP127ALUNO.LOCAL>
2022-03-23 11:19:50 -0300

    atualização

--- Teste.java
+++ Teste.java
@@ -1,3 +1,16 @@
 class Teste {
-    
+    public static void main(String args[]) {
+
+        int n = 1;
+
+        for(int i=20; i <= 50; i=i+1 ){
+            if(i == 50){
+                System.out.print("31°" + i + ".");
+            }
+            else{
+            System.out.print( n++ + "°" + i + ", ");
+            }
+
+        }
+    }
 }
